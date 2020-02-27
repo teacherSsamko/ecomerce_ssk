e15aefa4a62a72c32c63d59e77a290b507ee5be1
ssamko <teacher.ssamko@gmail.com>
2020-02-27 13:34:46 +0900

    orderlist page

--- fc_django/order/views.py
+++ fc_django/order/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.views.generic.edit import FormView
+from django.views.generic import ListView
 from .forms import RegisterForm
+from .models import Order
 # Create your views here.
 
 
@@ -17,3 +19,15 @@ class OrderCreate(FormView):
             'request': self.request
         })
         return kw
+
+
+class OrderList(ListView):
+    model = Order
+    template_name = "order.html"
+    context_object_name = 'order_list'
+
+    # session에서 해당 회원의 정보만 가져와서 orderlist에 출력하기 위해 함수 overriding
+    def get_queryset(self, **kwargs):
+        queryset = Order.objects.filter(
+            fcuser__email=self.request.session.get('user'))
+        return queryset
